33638e3de0a70c0dee9c4c398ea6b165256cc92e
ayaka010816 <ayaka.hostos@gmail.com>
2015-10-04 19:04:35 +0000

    user_item

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
+    gravatar_url = "https://secure.gravatar.com/avatar/ayaka.hostos@gmail.com?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
