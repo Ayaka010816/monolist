1835f31c00a7715895458b512bfe86f9a513c085
ayaka010816 <ayaka.hostos@gmail.com>
2015-10-08 23:33:28 +0000

    items.id-forHeroku

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,12 +1,12 @@
 class RankingController < ApplicationController
 
  def want
-  @item_id = Want.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
+  @item_id = Want.group('items.id').order("count_item_id DESC").limit(10).count('items.id').keys
   @items = Item.find(@item_id)
  end
  
  def have
-  @item_id = Have.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
+  @item_id = Have.group('items.id').order("count_item_id DESC").limit(10).count('items.id').keys
   @items = Item.find(@item_id)
  end   
  
