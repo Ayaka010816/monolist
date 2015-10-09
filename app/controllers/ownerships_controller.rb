5a496f6e9fdf61da39a0df8039f47e55de1b7ed6
ayaka010816 <ayaka.hostos@gmail.com>
2015-10-09 04:15:33 +0000

    ranking

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -18,7 +18,8 @@ class OwnershipsController < ApplicationController
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
-      
+
+      amazon_item       = response.items.first
       @item.title        = amazon_item.get('ItemAttributes/Title')
       @item.small_image  = amazon_item.get("SmallImage/URL")
       @item.medium_image = amazon_item.get("MediumImage/URL")
@@ -28,30 +29,24 @@ class OwnershipsController < ApplicationController
       @item.save!
     end
 
-    # TODO ユーザにwant or haveを設定する
-    # params[:type]の値ににHaveボタンが押された時には「Have」,
     if params[:type] == "Have"
       current_user.have(@item)
     end
-    # Wantボタンがされた時には「Want」が設定されています。
+    
     if params[:type] == "Want"
       current_user.want(@item)
     end
-    
   end
 
   def destroy
     @item = Item.find(params[:item_id])
-
-    # TODO 紐付けの解除。 
-    # params[:type]の値ににHavedボタンが押された時には「Have」,
     if params[:type] == "Have"
       current_user.unhave(@item)
     end
-    # Wantedボタンがされた時には「Want」が設定されています。
+    
     if params[:type] == "Want"
       current_user.unwant(@item)
     end
-
   end
+  
 end
