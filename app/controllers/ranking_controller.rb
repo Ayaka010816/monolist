b1f8fe87155fc3efac4a8f7b4bc43de1a0160a38
ayaka010816 <ayaka.hostos@gmail.com>
2015-10-10 18:55:10 +0000

    ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,13 +1,13 @@
 class RankingController < ApplicationController
 
- def want
-  @item_id = Want.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
-  @items = Item.find(@ids).sort_by{|o| @ids.index(o.id)}
- end
- 
- def have
-  @item_id = Have.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
-  @items = Item.find(@ids).sort_by{|o| @ids.index(o.id)}
- end   
- 
+    def have
+        @ids = Have.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
+        @items = Item.find(@ids).sort_by{|o| @ids.index(o.id)}
+    end
+    
+    def want
+        @ids = Want.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
+        @items = Item.find(@ids).sort_by{|o| @ids.index(o.id)}
+    end
+        
 end
